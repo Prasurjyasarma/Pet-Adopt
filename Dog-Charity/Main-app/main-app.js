5c111aa74d749a42919c54f18d78b13c7ee6edc9
Prasurjyasarma <prasurjyasarma9@gmail.com>
2023-12-22 12:03:02 +0530

    Initial Commit

--- Dog-Charity/Main-app/main-app.js
+++ Dog-Charity/Main-app/main-app.js
@@ -1,45 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //! Get the container element where the cards will be added
+  const preLoader = document.getElementById("preloader");
   const cardContainer = document.getElementById("cardContainer");
-  const detailsContainer = document.getElementById("detailsContainer");
 
+  //! Function to hide the preloader
+  window.addEventListener("load", () => {
+    preLoader.style.display = "none";
+  });
+
+  //! Fetch data from the JSON file
   fetch("../dog-data.json")
     .then((response) => response.json())
     .then((jsonData) => {
+      //! Iterate through each item in the JSON data
       jsonData.forEach((item) => {
+        //! Create a card element
         const card = document.createElement("div");
         card.classList.add("card");
 
+        //! Create an image element and set its source
         const image = document.createElement("img");
         image.src = item.imageUrl;
         card.appendChild(image);
 
+        //! Create a title element and set its text content
         const title = document.createElement("h2");
         title.textContent = item.name;
         card.appendChild(title);
 
+        //! Append the card to the card container
         cardContainer.appendChild(card);
+
+        //! Add a click event listener to each card
         card.addEventListener("click", function () {
-          detailsContainer.style.display = "block";
-          displayDetails(item);
+          //! Call the navigateToDetailsPage function when a card is clicked
+          navigateToDetailsPage(item);
         });
       });
     })
     .catch((error) => console.error("Error", error));
 
-  function displayDetails(item) {
-    detailsContainer.innerHTML = "";
+  //! Function to navigate to the details page with item information
+  function navigateToDetailsPage(item) {
+    //! Create a query string with item information
+    const queryParams = `?name=${item.name}&imageUrl=${item.imageUrl}`;
 
-    const detailsImage = document.createElement("img");
-    detailsImage.src = item.imageUrl;
-
-    const detailsTitle = document.createElement("h2");
-    detailsTitle.textContent = `Hi my name is ${item.name}`;
-
-    const detailsParagraph = document.createElement("p");
-    detailsParagraph.textContent = "Additional details about the dog...";
-
-    detailsContainer.appendChild(detailsImage);
-    detailsContainer.appendChild(detailsTitle);
-    detailsContainer.appendChild(detailsParagraph);
+    //! Set the window location to the details page with the query parameters
+    window.location.href = `./details-content/detailscontainer.html${queryParams}`;
   }
 });
